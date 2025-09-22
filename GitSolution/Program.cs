54905b7cb9692349b982cd1c8a5f1a173a7cf8ab
Mathias Norrback <mathias.norrbacken@gmail.com>
2025-09-22 16:29:27 +0200

    Change comment.

--- GitSolution/Program.cs
+++ GitSolution/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Detta är Mathias repo.
             //hellu
-            //hello
         }
     }
 }
