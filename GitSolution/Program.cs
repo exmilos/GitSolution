caa888a19182830220339c56445ea7531dfb01dd
mathi <mathi@MATHIAS>
2025-09-22 14:57:46 +0200

    Add project files.

---
+++ GitSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
